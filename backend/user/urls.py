5f8a54a09766eab7adb89eb52cbb6d8cfc2c333b
Peter Spolar <spolarpeter86@gmail.com>
2021-11-01 15:50:49 +0100

    functioning scheduler

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from user.views import ListAllUsersView, RetrieveUpdateDeleteUserView, SpecificUserView
+from user.views import ListAllUsersView, RetrieveUpdateProfileView, SpecificUserView
 
 urlpatterns = [
     path('users/list/', ListAllUsersView.as_view()),
-    path('users/me/', RetrieveUpdateDeleteUserView.as_view()),
+    path('users/me/', RetrieveUpdateProfileView.as_view()),
     path('users/?search=<str:search_string>/', ListAllUsersView.as_view()),
     path('users/<int:user_id>/', SpecificUserView.as_view())
 ]
