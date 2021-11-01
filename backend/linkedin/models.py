3b31eb01945c280d0e9202ec5c8db2dacf18a398
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2021-11-01 13:29:09 +0100

    endpoint for frontend linkedIn Auth

--- backend/linkedin/models.py
+++ backend/linkedin/models.py
@@ -6,7 +6,7 @@ class LinkedInPost(models.Model):
     content = models.TextField()
     link = models.TextField(blank=True, null=True)
     image = models.ImageField(blank=True, null=True)
-    author = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='inpost')
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='linkedin_posts')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     post_date_time = models.DateTimeField(blank=True, null=True)
